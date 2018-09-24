5c0c93c567d196996a2db4ec79d53b449cfaa4a2
Jennifer Walker <dataqueen16@gmail.com>
2018-09-24 14:24:36 -0500

    added authorization code in the database rules file for all who try to access the database

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,21 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function ($firebaseRefProvider) {
+  var config = {
+    apiKey: "AIzaSyCfsPgS0Om56NmLuhSqahuD1284blTLvhY",
+    authDomain: "contacts-manager-86066.firebaseapp.com",
+    databaseURL: "https://contacts-manager-86066.firebaseio.com",
+    projectId: "contacts-manager-86066",
+    storageBucket: "contacts-manager-86066.appspot.com",
+    messagingSenderId: "741436178153"
+  };
+  $firebaseRefProvider
+    .registerUrl({
+      default: config.databaseURL,
+      contacts: config.databaseURL + '/contacts'
+    });
+    
+  firebase.initializeApp(config);
+  })
